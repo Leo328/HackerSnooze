5e8c48de477972903b5ac010935a2b710d073b3b
Leo Ng <leogetsthingsdone@gmail.com>
2018-02-28 17:32:01 -0800

    update with hidden submit button if user isn't logged in

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 $(function() {
-  // var stories = [
-  //   {
-  //     name: "Google",
-  //     url: "http://www.google.com"
-  //   },
-  //   {
-  //     name: "Who the hell still uses this?",
-  //     url: "http://www.askjeeves.com"
-  //   },
-  //   {
-  //     name: "Facebook",
-  //     url: "http://www.facebook.com"
-  //   }
-  // ];
+  function setLayout() {
+    if (!isLoggedIn()) {
+      $("#submitLink").hide();
 
-  //   <i class="far fa-star"></i>
+      // show loginId/register form
+    } else {
+      $("#submitLink").show();
+      // hide login/reg
+    }
+  }
+  function isLoggedIn() {
+    var token = localStorage.token;
+    return typeof token === "string" && token.length ? true : false;
+  }
   function createList() {
     $.ajax({
       url: "https://hack-or-snooze.herokuapp.com/stories?skip=0&limit=10",
       dataType: "json",
-
       success: function(response) {
         console.log(response.data);
         var stories = response.data;
@@ -37,30 +34,79 @@ $(function() {
       type: "GET"
     });
   }
-
-
-  $('#signup-button').on('click', function () {
-    $.ajax() {
-      type: 'post',
-      dataType: 'json',
-      url: 'https://hack-or-snooze.herokuapp.com/users/mhueter',
-    }
-  })
-
-
-
+  //Step1
+  var $username = $("#LoginButtonDiv-submit").on("click", function() {
+    $.ajax("#Username");
+  });
+  $("#LoginButtonDiv").on("click", function() {
+    $("#signup-button").fadeToggle();
+    var $name = $("#Name").val();
+    var $userName = $("#Username").val();
+    var $password = $("#Password").val();
+    $.ajax({
+      method: "POST",
+      dataType: "json",
+      url: "https://hack-or-snooze.herokuapp.com/users",
+      headers: {
+        Accept: "application/json"
+      },
+      data: {
+        data: {
+          username: $userName,
+          name: $name,
+          password: $password
+        }
+      }
+    }).then(function(response) {
+      console.log(response);
+    });
+  });
+  $("#Login").on("click", function() {
+    var $userName = $("#Username").val();
+    var $password = $("#Password").val();
+    $.ajax({
+      method: "POST",
+      url: "https://hack-or-snooze.herokuapp.com/auth",
+      data: {
+        data: {
+          username: $userName,
+          password: $password
+        }
+      }
+    }).then(function(auth) {
+      localStorage.token = auth.data.token;
+      localStorage.username = $userName;
+      setLayout();
+    });
+  });
   $("#story-form").on("submit", function(e) {
     e.preventDefault();
     var inputName = $("#inputName").val();
     var inputUrl = $("#inputUrl").val();
-    stories.push({
-      name: inputName,
-      url: inputUrl
-    });
+    // stories.push({
+    //   name: inputName,
+    //   url: inputUrl
+    // });
     // console.log(stories);
+    $.ajax({
+      method: "POST",
+      url: "https://hack-or-snooze.herokuapp.com/stories?skip=0&limit=10",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      data: {
+        data: {
+          username: localStorage.username,
+          title: inputName,
+          author: localstorage.username,
+          url: inputUrl
+        }
+      }.then(function() {
+        createList();
+      })
+    });
     createList();
   });
-
   $("#stories").on("click", ".fa-star", function() {
     if ($(this).hasClass("far")) {
       $(this)
@@ -73,34 +119,6 @@ $(function() {
     }
   });
   console.log(stories);
-
+  setLayout();
   createList();
 });
-
-// function createList() {
-//     $('#stories').empty();
-//     for (var i = 0; i < stories.length; i++) {
-//       $("#stories").append("<li><i class='far fa-star'></i> " + stories[i].name + " <span class='text-secondary'>(" + stories[i].url + ")</span></li>");
-//     }
-//   }
-//   $('#story-form').on('submit', function (e) {
-//     e.preventDefault();
-//     stories.push({
-//       name: $('#inputName').val(),
-//       url: $('#inputUrl').val()
-//     });
-//     console.log(stories);
-//     createList();
-//   });
-//   $('#stories').on('click', '.fa-star', function () {
-//     if ($(this).hasClass('far')) {
-//       $(this).removeClass('far').addClass('fas');
-//     }
-//     else {
-//       $(this).removeClass('fas').addClass('far');
-//     }
-//   })
-//   console.log(stories);
-
-//   createList();
-// });
